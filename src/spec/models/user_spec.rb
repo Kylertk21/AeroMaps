a59f1564defe750827eaac4796fad4b86c52c08d
kyler <kyler@kramer.kramer>
2024-11-09 01:35:46 -0700

    capybara feature tests started

--- src/spec/models/user_spec.rb
+++ src/spec/models/user_spec.rb
@@ -18,7 +18,8 @@ FactoryBot.define do
     end
 end
 
-RSpec.describe "Users", type: :request do 
+# Rspec Tests
+RSpec.describe "User model authentication", type: :request do 
   
   describe "POST /users/new" do # Create a new user and check that it redirects
     context "with valid parameters" do 
@@ -33,7 +34,7 @@ RSpec.describe "Users", type: :request do
   
   describe "GET /users" do # Check that user is authenticated and can access home page
     it "returns a success response" do
-      user = FactoryBot.create(:user)
+      user = FactoryBot.build(:user)
       sign_in user
 
       get home_index_path
@@ -44,7 +45,7 @@ RSpec.describe "Users", type: :request do
     end
 
   context "with invalid parameters" do 
-    
+
     it "does not create a new user with empty fields" do
       expect { 
         post user_registration_path, params: { user: attributes_for(:user, :empty) }
@@ -66,3 +67,50 @@ RSpec.describe "Users", type: :request do
     end
   end
 end
+
+# Feature tests
+
+RSpec.feature "User Registration", type: :feature do
+  scenario "User signs up with valid credentials" do 
+    user = FactoryBot.build(:user)
+
+    visit new_user_registration_path 
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password_confirmation
+
+    click_button "Sign up"
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content("Home#index")
+  end
+end
+
+
+feature "User signs in", type: :feature do
+    user = FactoryBot.build(:user)
+    empty_user = FactoryBot.build(:user, :empty)
+ 
+   scenario "User signs in with correct credentials" do 
+    visit '/users/sign_in'
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_button "Log in"
+
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content("Home#index")
+  end
+
+  scenario "User can't sign in with invalid data" do
+    visit '/users/sign_in'
+      fill_in "Email", with: empty_user.email
+      fill_in "Password", with: empty_user.password
+      click_button "Log in"
+    
+      expect(page).to have_current_path(user_session_path)
+      expect(page).to have_content("Invalid Email or password")
+
+  end
+end
+
