c2e869234804762c1e60f7bafc7f5560034dbf15
kyler <kyler@kramer.kramer>
2024-11-08 23:04:04 -0700

    authentication works, now need to clean up views and controllers

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validate :email_format
+         def email_format
+             unless email =~ /\A[-\w!#$%&'*+=?`{|}~^]+(?:\.[\w!#$%&'*+=?`{|}~^-]+)*@[a-zA-Z0-9-]+\.[a-zA-Z]{2,6}\z/
+                 errors.add(:email, "must be a valid email address")
+             end
+         end
+      
 end
