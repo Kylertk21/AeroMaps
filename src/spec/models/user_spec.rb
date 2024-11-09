7f83c816fb8591de081ff646c75f767dcd9f6b38
kyler <kyler@kramer.kramer>
2024-11-09 00:45:12 -0700

    added factorybot to tests and completed sad-path tests for user authentication, passed

--- src/spec/models/user_spec.rb
+++ src/spec/models/user_spec.rb
@@ -1,23 +1,42 @@
 require 'rails_helper'
 
+FactoryBot.define do
+  factory :user do 
+    email { "test@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    end
+  trait :empty do
+    email { "" }
+    password { "" }
+    password_confirmation { "" }
+    end
+  trait :invalidpw do 
+    email { "test@example.com" }
+    password { "password" }
+    password_confirmation { "otherpassword" }
+    end
+end
+
 RSpec.describe "Users", type: :request do 
   
   describe "POST /users/new" do # Create a new user and check that it redirects
     context "with valid parameters" do 
       it "creates a new user" do
         expect {
-          post users_path, params: { user: { email_address: "test@gmail.com", password: "password"} }
+          post user_registration_path, params: { user: attributes_for(:user) } 
       }.to change(User, :count).by(1)
-
-      expect(response).to have_http_status(:found)
-      follow_redirect!
-      expect(response.body).to include("test@gmail.com")
+      
+      expect(response).to have_http_status(:see_other)
       
       end
   
-  describe "GET /users" do # Check that /users returns successfully
+  describe "GET /users" do # Check that user is authenticated and can access home page
     it "returns a success response" do
-      get users_path 
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      get home_index_path
       expect(response).to have_http_status(:ok)
     
         end
@@ -25,15 +44,25 @@ RSpec.describe "Users", type: :request do
     end
 
   context "with invalid parameters" do 
-    it "does not create a new user and returns 422" do
+    
+    it "does not create a new user with empty fields" do
       expect { 
-        post users_path, params: { user: { email_address:"", password:""} }
+        post user_registration_path, params: { user: attributes_for(:user, :empty) }
     }.to_not change(User, :count)
     
     expect(response).to have_http_status(422)
 
       end
+      
+    it "does not create a new user with mismatched password fields" do
+      expect { 
+        post user_registration_path, params: { user: attributes_for(:user, :invalidpw) }
+    }.to_not change(User, :count)
+      
+    expect(response).to have_http_status(422)
+  
+      end
+
     end
   end
 end
-#TODO test user authentication with devise
