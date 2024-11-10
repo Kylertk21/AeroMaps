2f2dcf1ae031f0a97465d49531e45763fa2e33af
kyler <kyler@kramer.kramer>
2024-11-09 18:43:17 -0700

    finished initial feature tests for pilot profile creation

--- src/spec/models/pilot_profiles_spec.rb
+++ src/spec/models/pilot_profiles_spec.rb
@@ -52,10 +52,7 @@ feature "Pilot Profile creation", type: :feature do
     user = FactoryBot.create(:user)
     pilot = FactoryBot.create(:pilot_profile)
 
-    
-
-
-    scenario "Pilot creates profile with valid credentials" do
+    scenario "with valid pilot credentials" do
 
       # Log In
       visit "/users/sign_in"
@@ -63,7 +60,7 @@ feature "Pilot Profile creation", type: :feature do
       fill_in "Password", with: user.password
       click_button "Log in"
 
-      # Fill out form fields ad create pilot profile
+      # Fill out form fields and create pilot profile
       visit "/pilot_profiles/new"
       fill_in "First name", with: pilot.first_name
       fill_in "Last name", with: pilot.last_name 
@@ -78,5 +75,24 @@ feature "Pilot Profile creation", type: :feature do
       expect(page).to have_current_path(pilot_profile_path(PilotProfile.last))
       expect(page).to have_content("Pilot profile was successfully created.")
     end
-  end
 
+    scenario "With invalid pilot credentials" do 
+      visit "/users/sign_in"
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_button "Log in"
+
+      visit "/pilot_profiles/new"
+      fill_in "First name", with: ""
+      fill_in "Last name", with: ""
+      fill_in "Aircraft ident", with: ""
+      fill_in "Aircraft type", with: ""
+      fill_in "Home address", with: ""
+      fill_in "Phone number", with: ""
+      fill_in "Bio", with: ""
+
+      click_button "Create Pilot profile"
+      
+      expect(page).to have_content("7 errors prohibited this pilot_profile from being saved:")
+  end
+end
