e199ef8fac6e8b00695b96cf6640fde3b0dbc4a6
kyler <kyler@kramer.kramer>
2024-11-10 00:19:28 -0700

    METAR data now displays on map

--- src/app/assets/javascript/map.js
+++ src/app/assets/javascript/map.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     // Retrieve METAR data from show.html
+    var mapContainer = document.getElementById("map");
+    if (!mapContainer) {
+        console.error("Map container not found");
+        return;
+    }
+
     var metarDataElement = document.getElementById("metar");
     if (!metarDataElement){
         console.error("METAR data element not found");
@@ -11,14 +17,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
     try {
         metarData = JSON.parse(metarDataElement.getAttribute("metar-data"));
+        console.log("METAR data:", metarData);
     } catch (e) {
         console.error("Failed to parse METAR data:", e);
         metarData = "No data available";
     }
 
+    var lat = 38.8017 
+    var lng = -104.7026
+    console.log("Coordinates:", lat, lng)
+
     // map initialization
-    var map = L.map('map').setView([38.8017, -104.7026], 13); // Initial coordinates
-    map.setView();
+    var map = L.map('map').setView([lat, lng], 13); // Initial coordinates
 
     // Set up OpenStreetMap
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -27,11 +37,15 @@ document.addEventListener("DOMContentLoaded", function() {
     }).addTo(map);
 
     // Add maker with METAR data
-    var marker = L.marker([38.8017, -104.7026]).addTo(map);
+    var marker = L.marker([lat, lng]).addTo(map);
     var popupContent = `<b>Colorado Springs Airport 
                         (KCOS)</b><br>METAR: 
                         ${metarData ? JSON.stringify(metarData, null, 2) 
                             : 'No data available' } `;
     
     marker.bindPopup(popupContent).openPopup();
+
+    setTimeout(function() {
+        map.invalidateSize();
+    }, 100);
 });
